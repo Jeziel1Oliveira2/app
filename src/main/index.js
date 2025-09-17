295b2db68aad47b477934a29533fe5e2c2116bed
Jeziel Oliveira de Paula <jeziel.oliveira.paula@gmail.com>
2025-09-17 19:55:14 -0300

    fiz a function do IMC, so falta estilizar

--- src/main/index.js
+++ src/main/index.js
@@ -1,32 +1,67 @@
 import logo from '../logo.svg';
 import './index.css';
+import { useState } from 'react';
+
+
+function imc (peso, altura) {
+  let mult = altura * altura;
+  let div = peso / mult;
+  return div.toFixed(2)
+};
 
 function App() {
+  let [peso, setPeso] = useState('');
+  let [altura, setAltura] = useState('');
+
+  let [resultadoImc, setResultadoImc] = useState(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let calcularImc = imc(peso, altura);
+    setResultadoImc(calcularImc);
+  };
+
   return (
     <div className="App">
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
           <div>
-            <form>
+            <form onSubmit={handleSubmit}>
               <label>
                   Digite seu nome:
               </label>
-                <input type="text" name='Nome'>
-                </input>
-                <input type='submit' value='enviar'>
-                </input>
+              <input type="text" name='Nome' />
+              <label>
+                  Digite sua idade:
+              </label>
+              <input type="number" name='Idade'/>
+              <label>
+                  Digite seu peso:
+              </label>
+              <input 
+                type="number" 
+                name='Peso'
+                value={peso} 
+                onChange={(e) => setPeso(e.target.value)} 
+              />
+              <label>
+                Digite sua altura:
+              </label>
+              <input 
+                type='number' 
+                name='Altura'
+                value={altura}
+                onChange={(e) => setAltura(e.target.value)}
+              />
+              
+              <input type='submit' value='Enviar'>
+              </input>
             </form>
+          </div>
+
+          <div>
+            <p>
+              Seu IMC é: {resultadoImc}
+            </p>
           </div>
       </div>
     </div>
