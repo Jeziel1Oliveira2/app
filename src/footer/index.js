b23c56673277e1eae41527fe1e6b90f314af85b7
Jeziel Oliveira de Paula <jeziel.oliveira.paula@gmail.com>
2025-09-20 04:21:21 -0300

    criei o footer

---
+++ src/footer/index.js
@@ -0,0 +1,41 @@
+function footer() {
+    return(
+        <footer>
+            <div>
+                <li>
+                    <ul>
+                        Criado por: Jeziel Oliveira de Paula
+                    </ul>
+                    <ul>
+                        linkedin:
+                       <a 
+                       href="https://www.linkedin.com/in/jeziel-oliveira-de-paula-21b8211b3"
+                       target="_blank"
+                       rel="noopener noreferrer"
+                       >
+                        https://www.linkedin.com/in/jeziel-oliveira-de-paula-21b8211b3
+                       </a>  
+                    </ul>
+                    <ul>
+                        GitHub:
+                        <a 
+                       href="https://github.com/Jeziel1Oliveira2"
+                       target="_blank"
+                       rel="noopener noreferrer"
+                       >
+                        https://github.com/Jeziel1Oliveira2
+                       </a>
+                    </ul>
+                    <ul>
+                        E-mail: jeziel.oliveira.paula@gmail.com
+                    </ul>
+                    <ul>
+                        Telefone: (51) 99649-8095
+                    </ul>
+                </li>
+            </div>
+        </footer>
+    )
+}
+
+export default footer;
